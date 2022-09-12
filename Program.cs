f96cb57d873d54181b34245db9dc6f18f5272414
wang.weiran <wang.wei.ran@outlook.com>
2022-09-13 01:16:50 +1000

    add vue static file folder

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
