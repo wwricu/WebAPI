66f293abfb717a87a7389432d7ec3f9e9b268631
wang.weiran <wang.wei.ran@outlook.com>
2022-09-14 04:15:47 +1000

    integrate identity

---
+++ Entity/TestUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SqlSugar;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Entity
+{
+    public class TestUser : IdentityUser
+    {
+        [Required]
+        [StringLength(128)]
+        [SugarColumn(ColumnDataType = "varchar(64)")]
+        public string? Address { get; set; }
+    }
+}
