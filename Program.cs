fa14e3619d06dd1a407092cd4383995526488bfb
wang.weiran <wang.wei.ran@outlook.com>
2022-09-14 19:31:03 +1000

    integrate identity

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Http;
-using System.IO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Security;
+using WebAPI.Entity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddIdentityCore<TestUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 
