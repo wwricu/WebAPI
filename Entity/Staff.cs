65a6e00f5ec9b7457e5f64fbb64351bd1655b679
wang.weiran <wang.wei.ran@outlook.com>
2022-09-15 14:52:23 +1000

    remove identity

--- Entity/Staff.cs
+++ Entity/Staff.cs
@@ -1,25 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
-using SqlSugar;
+﻿using SqlSugar;
 
 namespace WebAPI.Entity
 {
-    public class Staff : IdentityUser
+    public class Staff
     {
-        [PersonalData]
         public string? StaffNumber { get; set; }
-        [PersonalData]
         public string? StaffName { get; set; }
-        [PersonalData]
         public DateTime Birthdate { get; set; }
-        [PersonalData]
         public string? AddressLine1 { get; set; }
-        [PersonalData]
         public string? AddressLine2 { get; set; }
-        [PersonalData]
         public string? AddressLine3 { get; set; }
-        [PersonalData]
         public string? Faculty { get; set; }
-        [PersonalData]
         public int Status { get; set; }
 
         [Navigate(typeof(PrivilegeStaffMapping),
--- Entity/Student.cs
+++ Entity/Student.cs
@@ -1,31 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using SqlSugar;
+﻿using SqlSugar;
 
 
 namespace WebAPI.Entity
 {
-    public class Student : IdentityUser
+    public class Student
     {
-        [Required]
-        [StringLength(128)]
         public string? Address { get; set; }
 
-        [PersonalData]
         public string? StudentNumber { get; set; }
-        [PersonalData]
         public string? StudentName { get; set; }
-        [PersonalData]
         public DateTime BirthDate { get; set; }
-        [PersonalData]
         public string? AddressLine1 { get; set; }
-        [PersonalData]
         public string? AddressLine2 { get; set; }
-        [PersonalData]
         public string? AddressLine3 { get; set; }
-        [PersonalData]
         public string? AcademicProgram { get; set; }
-        [PersonalData]
         public int Status { get; set; }
 
         [Navigate(typeof(StudentOfferingMapping),
