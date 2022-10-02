e7c3e51b60a36b4838cf8df10c5a4842ca6cddfc
wang.weiran <wang.wei.ran@outlook.com>
2022-10-02 21:23:47 +1100

    modify application

--- Entity/Application.cs
+++ Entity/Application.cs
@@ -5,28 +5,32 @@ namespace WebAPI.Entity
 {
     public class Application
     {
+        public Application() { }
         public Application(AssessmentInstance assessment)
         {
-            Type = "Formal Exam";
-            Status = "Pending";
             AssessmentID = assessment.AssessmentID;
             StudentID = assessment.StudentID;
+            Type = "Formal Exam";
+            Status = "Pending";
             SubmitDate = DateTime.Now.ToString("dd-MM-yyyy");
         }
         [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
         public int ApplicationID { get; set; }
         [SugarColumn(ColumnDataType = "varchar(100)")]
-        public string Type { get; set; }
+        public string? Type { get; set; }
         [SugarColumn(ColumnDataType = "varchar(10000)")]
         public string? Documentation { get; set; }
         public string? Descriptipn { get; set; }
-        public string SubmitDate { get; set; }
-        public string Status { get; set; } // Pending, NMI, Approved, Rejected
+        public string? SubmitDate { get; set; }
+        public string? Status { get; set; } // Pending, NMI, Approved, Rejected
         public int StudentID { get; set; }
         [Navigate(NavigateType.OneToOne, nameof(StudentID))]
         public SysUser? Student { get; set; }
-        public string? AssessmentID { get; set; }
-        [Navigate(NavigateType.OneToOne, nameof(AssessmentID))]
-        public Assessment? Assessment { get; set; }
+        public int StaffID { get; set; }
+        [Navigate(NavigateType.OneToOne, nameof(StaffID))]
+        public SysUser? Staff { get; set; }
+        public string? InstanceID { get; set; } // instance
+        [Navigate(NavigateType.OneToOne, nameof(InstanceID))]
+        public AssessmentInstance? AssessmentInstance { get; set; }
     }
 }
